6c77f5df93e0d854f93b5fc7daef4f6f72dbff12
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-05-06 19:57:42 +0300

    Template-Driven Forms examined, notted

--- AngularTutorial/12_15-Forms/src/app/app.component.ts
+++ AngularTutorial/12_15-Forms/src/app/app.component.ts
@@ -1,10 +1,43 @@
+import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+   <h3>Template-driven Form</h3>
+   <form #frm="ngForm" (ngSubmit)="onSubmit(frm.value)">
+
+      <input type="text" name="name" placeholder="Name"  ngModel> <br>  
+      <input type="text" name="surname" placeholder="Surname"  ngModel>  <br> 
+      <input type="text" name="email" placeholder="Email" ngModel>   <br>
+      <!-- // If tel wouldn't be a ngModel, it wouldn't be included in the form data -->
+      <input type="text" name="tel" placeholder="Tel" ngModel> <br>  
+
+      <div ngModelGroup="address"> 
+          <input type="text" name="country" placeholder="Country" ngModel> <br>
+          <input type="text" name="city" placeholder="City" ngModel> <br>
+          <input type="text" name="address" placeholder="Address" ngModel> <br>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  `,
 })
 export class AppComponent {
-  title = '12_15-Forms';
+
+  @ViewChild("frm", { static: true }) frm : NgForm
+
+  onSubmit(data: any) {
+    console.log(this.frm.value); // Returns the all FormControls in the FormGroup
+    // {name: 'Hakan', surname: 'Yavaş', email: 'admin@gmail.com', tel: '543 3054301'}
+
+    console.log(this.frm.valid); // Returns the validation status of the form
+    
+    console.log(this.frm.touched); // Returns the touched status of the form
+
+    console.log(this.frm.submitted); // Returns the submitted status of the form
+
+    console.log(data);
+  }
 }
