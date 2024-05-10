7589c25d05058972842ac1a619a4037890f35923
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-05-10 16:19:27 +0300

    Location strategies notted

--- AngularTutorial/22_29-Routing/src/app/app.component.ts
+++ AngularTutorial/22_29-Routing/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +6,18 @@ import { Component } from '@angular/core';
   <a routerLinkActive="active" routerLink="home">Home</a> | <a routerLinkActive="active" routerLink="about">About</a> | <a routerLinkActive="active" [routerLink] = "['contact','a','b','c']">Contact</a>
   <hr>
   <router-outlet></router-outlet> <!-- This is where the router will display the components -->
+  <br>
+  <button (click)="navigate()">Navigate</button>
   `,
   styles: [
     ".active{color:red;}"
   ]
-  
+
 })
 export class AppComponent {
+  navigate() {
+    history.pushState({ message: 'Hello' }, "title", '/a/b')
+  }
+
   title = '22_29-Routing';
 }
