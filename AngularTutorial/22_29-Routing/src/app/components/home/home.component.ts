fd5d8c7b7e9ced69df70e82fb60dfadcfcb8d365
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-05-10 17:23:34 +0300

    Route parameters examined, notted on Notion

--- AngularTutorial/22_29-Routing/src/app/components/home/home.component.ts
+++ AngularTutorial/22_29-Routing/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
  
 })
 export class HomeComponent {
+  // ActivatedRoute is used to get the current route's information
+  /*
+  constructor(private activatedRoute : ActivatedRoute){
+    var d = this.activatedRoute.snapshot.paramMap.has("id");
+    var e = this.activatedRoute.snapshot.paramMap.get("id");
+    console.log("id",d, e);
+  }
+  */
+
+  // Using Observable
+  constructor(private activatedRoute : ActivatedRoute){
+    this.activatedRoute.paramMap.subscribe(params => {
+      var d = params.has("id");
+      var e = params.get("id");
+      console.log("id",d, e);
+    });
+    
+    this.activatedRoute.params.subscribe({
+      next: (parameter) => {
+       console.log("parameter", parameter);
+      }
+    })
+
+  }
 
 }
