28692100b4c0baae3215c7a4ba643c8ceea3c01c
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-05-10 17:44:54 +0300

    Child and nested routes examined, notted

---
+++ AngularTutorial/22_29-Routing/src/app/components/product/product-detail/count/count.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-count',
+  template: `
+    <h1>Count</h1>
+    <p>Count: 0</p>
+    <button>Increment</button>
+    <button>Decrement</button>
+  `
+
+})
+export class CountComponent {
+
+}
