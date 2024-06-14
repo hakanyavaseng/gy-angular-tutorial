179479fa5955627a4909744842cdbb50a376b6fe
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-06-14 19:26:05 +0300

    ng-container covered

--- AngularTutorial/35.NgContainer/src/app/app.component.ts
+++ AngularTutorial/35.NgContainer/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '35.NgContainer';
+
+  products : any = [
+    { id : 1, name : "Phone", available: true},
+    { id : 2, name : "Laptop", available: false},
+    { id : 3, name : "Tablet", available: true},
+    { id : 4, name : "Watch", available: false},
+    { id : 5, name : "Camera", available: true},
+    { id : 6, name : "Headphone", available: true},
+    { id : 7, name : "Keyboard", available: false},
+    { id : 8, name : "Mouse", available: true},
+    { id : 9, name : "Monitor", available: false},
+    { id : 10, name : "Speaker", available: true} 
+  ]
 }
