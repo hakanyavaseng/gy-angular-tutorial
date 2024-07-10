a6e102441b2ff4c6ee9c5f161260b2a512d6077d
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-07-10 21:54:47 +0300

    Control flow syntax covered for directives ngIf,ngFor and ngSwitch

--- AngularTutorial/51.ControlFlowSyntax/src/app/app.component.ts
+++ AngularTutorial/51.ControlFlowSyntax/src/app/app.component.ts
@@ -5,9 +5,43 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>{{title}}</h1>
+    
+    @if(condition)
+      {
+        <h2>True</h2>
+      }
+    @else {
+      <h2>False</h2>
+    }
+    ------------------------------
+    @for(number of numbers; track number; let i = $index; let f = $first; let l = $last;) {
+      <p>{{number}}</p>
+      <p>{{f}}</p>
+      <p>{{l}}</p>
+    }
+    @empty {
+          <!--  if numbers is empty -->
+      <p>No numbers</p>
+    
+    }
+    ------------------------------
+    @switch (numbers.length) {
+      @case(1) {
+        <p>One</p>
+      }
+      @case(2) {
+        <p>Two</p>
+      }
+      @default {
+        <p>Many</p>
+      }
+    }
+    `
 })
 export class AppComponent {
+  numbers = [1, 2];
+  condition: boolean = false;
   title = '51.ControlFlowSyntax';
 }
