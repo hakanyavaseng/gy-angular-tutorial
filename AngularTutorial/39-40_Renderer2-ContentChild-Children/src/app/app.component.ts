c2b2ce8e9423faf7b68498dc2abe063f31b350b2
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-07-14 18:38:26 +0300

    HTML manipulation with Renderer2 covered

--- AngularTutorial/39-40_Renderer2-ContentChild-Children/src/app/app.component.ts
+++ AngularTutorial/39-40_Renderer2-ContentChild-Children/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1 #h>{{ title }}</h1>
+
+  `,
+ 
 })
 export class AppComponent {
   title = '39-40_Renderer2-ContentChild-Children';
+
+  constructor(private renderer : Renderer2){}
+
+  @ViewChild("h", {static: true}) h : ElementRef;
+
+  ngOnInit(): void {
+    this.renderer.setStyle(this.h.nativeElement, "color", "red");
+  }
 }
