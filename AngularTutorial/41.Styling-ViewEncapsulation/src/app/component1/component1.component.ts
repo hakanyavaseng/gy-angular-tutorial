a72b47e9c6641e50b8f60c8ce09adfe23e24936c
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-07-14 20:33:51 +0300

    styles covered on notion

---
+++ AngularTutorial/41.Styling-ViewEncapsulation/src/app/component1/component1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  standalone: true,
+  imports: [],
+  templateUrl: './component1.component.html',
+  styleUrl: './component1.component.css'
+})
+export class Component1Component {
+
+}
---
+++ AngularTutorial/41.Styling-ViewEncapsulation/src/app/component2/component2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  standalone: true,
+  imports: [],
+  templateUrl: './component2.component.html',
+  styleUrl: './component2.component.css'
+})
+export class Component2Component {
+
+}
